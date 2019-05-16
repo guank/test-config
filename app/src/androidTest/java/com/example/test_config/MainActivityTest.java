de17037f0d4d3826bf3a416f07724e9f82f96504
KG <guank@users.noreply.github.com>
2019-05-16 12:26:25 -0700

    added ui test

---
+++ app/src/androidTest/java/com/example/test_config/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.example.test_config;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    public static final String NAME = "Hello World!";
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickItem_OpensTestActivity(){
+        onView(withId(R.id.testView))
+                .check(matches(withText(NAME)));
+    }
+
+}
